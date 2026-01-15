172eb01ee8e886efc88ce06d4471b6b001feda7d
enoch-bolarinwa <enoch-bolarinwa@>
2026-01-15 12:15:19 +0100

    index

--- src/js/product.js
+++ src/js/product.js
@@ -4,24 +4,24 @@ import ProductData from './ProductData.mjs'
 const dataSource = new ProductData('tents')
 
 function addProductToCart(product) {
-  let cart = getLocalStorage("cart");
+  let cart = getLocalStorage('cart')
 
   if (!Array.isArray(cart)) {
-    cart = [];
+    cart = []
   }
 
-  cart.push(product);
+  cart.push(product)
 
-  setLocalStorage("cart", cart);
+  setLocalStorage('cart', cart)
 }
 
 function getLocalStorage(key) {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : null;
+  const data = localStorage.getItem(key)
+  return data ? JSON.parse(data) : null
 }
 
 function setLocalStorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
+  localStorage.setItem(key, JSON.stringify(value))
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
