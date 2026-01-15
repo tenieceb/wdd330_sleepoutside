90bdc793ba61e0b03e4496b50fe7d462d147902d
enoch-bolarinwa <enoch-bolarinwa@>
2026-01-15 12:31:01 +0100

    product

--- src/js/product.js
+++ src/js/product.js
@@ -1,28 +1,19 @@
-import { setLocalStorage } from './utils.mjs'
+import { getLocalStorage, setLocalStorage } from './utils.mjs'
 import ProductData from './ProductData.mjs'
 
 const dataSource = new ProductData('tents')
 
 function addProductToCart(product) {
-  let cart = getLocalStorage('cart')
+  let cart = getLocalStorage('so-cart')
 
   if (!Array.isArray(cart)) {
     cart = []
   }
 
   cart.push(product)
-
-  setLocalStorage('cart', cart)
+  setLocalStorage('so-cart', cart)
 }
 
-function getLocalStorage(key) {
-  const data = localStorage.getItem(key)
-  return data ? JSON.parse(data) : null
-}
-
-function setLocalStorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value))
-}
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id)
@@ -30,4 +21,6 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document.getElementById('addToCart').addEventListener('click', addToCartHandler)
+document
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler)
