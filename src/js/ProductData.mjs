1ff73b4d8483a1b4e3029121dbb59dc6db772137
tenieceb <tabusenbark@gmail.com>
2026-01-16 20:00:15 -0700

    Created a dynamically loading product page for the tent products. The week 2 team activity

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -2,11 +2,11 @@ function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error("Bad Response");
+    throw new Error('Bad Response');
   }
 }
 
-export default class ProductData {
+export default class ProductData  {
   constructor(category) {
     this.category = category;
     this.path = `../json/${this.category}.json`;
@@ -17,7 +17,7 @@ export default class ProductData {
       .then((data) => data);
   }
   async findProductById(id) {
-    const products = await this.getData();
+    const products = await this.getData()
     return products.find((item) => item.Id === id);
   }
 }
