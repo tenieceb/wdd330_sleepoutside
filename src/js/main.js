ef93625c93d63fe095b4e79d95249c336189702d
tenieceb <tabusenbark@gmail.com>
2026-01-21 15:56:19 -0700

    team week3 project

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 import ProductData from './ProductData.mjs'
 import ProductList from './ProductList.mjs'
+import { loadHeaderFooter } from './utils.mjs'
 
 const dataSource = new ProductData('tents')
 
 const productList = new ProductList(
   'tents',
   dataSource,
-  document.querySelector('.product-list')
+  document.querySelector('.product-list'),
 )
 productList.init()
+
+loadHeaderFooter()
